7c3d71c304f97ac69ddaae69d606375a4b121ffa
praneethkumarpidugu <pidugu2007@gmail.com>
2015-11-30 23:28:12 -0500

    iterating through the models data objects through views from home.html

--- src/CMS/views.py
+++ src/CMS/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+from videos.models import Video
 
 def home(request):
     name = "Praneeth"
+    videos = Video.objects.all()
+    embeds = []
+    for vid in videos:
+        embeds.append("%s" % (vid.embed_code))
     context = {
         "the_name" : name,
-        "number" : 12,
+        "number" : videos.count(),
+        "videos" : videos,
+        "the_embeds" : embeds,
     }
     return render(request, "home.html", context)
     #return render_to_response("home.html", context, context_instance=RequestContext(request))
