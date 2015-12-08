9c8f81b90b71b06181fbb1bdc7cd73572386c28d
praneethkumarpidugu <pidugu2007@gmail.com>
2015-12-07 23:12:47 -0500

    added a url id regex

--- src/videos/views.py
+++ src/videos/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
+
+from .models import Video
+
+
 
 # Create your views here.
+def video_detail(request, id):
+    try:
+        obj = Video.objects.get(id=id)
+        return render(request,"videos/video_detail.html", {"obj": obj})
+    except:
+        raise Http404
+
+def video_list(request):
+    queryset = Video.objects.all()
+    context = {
+        "queryset" : queryset,
+    }
+    return render(request,"videos/video_list.html", context)
+
+# def video_edit(request):
+#     return render(request,"videos/video_detail.html", {})
+#
+# def video_create(request):
+#     return render(request,"videos/video_detail.html", {})
+
