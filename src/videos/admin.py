0f019763ecba31fdc767067ada3bd3120412f6f4
praneethkumarpidugu <pidugu2007@gmail.com>
2015-12-15 22:28:02 -0500

    Got rid of prepolated fields with post_signals to display the slug

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Video, Category
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", 'slug']
+    fields = ['title', 'embed_code',
+                 'active', 'featured',
+                 'free_preview', 'category']
+
+    #prepopulated_fields = {'slug': ['title'],}
+
+    class Meta:
+        model = Video
+
+
 admin.site.register(Category)
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
+
+
