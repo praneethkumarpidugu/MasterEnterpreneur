f5fd0e0442d766c8aff57f9d1ab7301f75a11e5f
praneethkumarpidugu <pidugu2007@gmail.com>
2016-02-06 01:08:17 -0500

    membership dates and mock transactions to upgrade

--- src/billing/models.py
+++ src/billing/models.py
@@ -1,8 +1,11 @@
+import datetime
 import random
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils import timezone
+
+from .signals import membership_dates_update
 
 # Create your models here.
 class Membership(models.Model):
@@ -29,6 +32,23 @@ def update_membership_status(sender, instance, created, **kwargs):
 
 post_save.connect(update_membership_status, sender=Membership)
 
+def update_membership_dates(sender, new_date_start, **kwargs):
+    membership = sender
+    current_date_end = membership.date_end
+    if current_date_end > new_date_start:
+        #append new_start_date plus offset to date end of the instance
+        membership.date_end = current_date_end + datetime.timedelta(days=30, hours=10)
+        membership.save()
+    else:
+        #set a new start date and new end date with the same offset.
+        membership.date_start = new_date_start
+        membership.date_end = new_date_start + datetime.timedelta(days=30, hours=10)
+        membership.save()
+    # membership.update_status()
+
+
+membership_dates_update.connect(update_membership_dates)
+
 
 class TransactionManager(models.Manager):
     def create_new(self, user, transaction_id, amount, card_type,\
@@ -53,7 +73,7 @@ class TransactionManager(models.Manager):
         if last_four is not None:
             new_trans.last_four = last_four
         new_trans.save(using=self._db)
-        return new_trans.order_id
+        return new_trans
 
 
 class Transaction(models.Model):
--- src/billing/views.py
+++ src/billing/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from .models import Transaction
+from .models import Transaction, Membership
+from .signals import membership_dates_update
 # Create your views here.
 import random
 
 def upgrade(request):
-    Transaction.objects.create_new(request.user, "asdafjafhjahf%s"%(random.randint(0,100)), 25.00, "visa")
+    if request.user.is_authenticated():
+        trans = Transaction.objects.create_new(request.user,\
+                                               "asdafjafhjahf%s"%(random.randint(0,100)),\
+                                               25.00, "visa")
+        if trans.success:
+            membership_instance, created = Membership.objects.get_or_create(user=request.user)
+            membership_dates_update.send(membership_instance, new_date_start = trans.timestamp)
     return render(request, "billing/upgrade.html", {})
 
 def billing_history(request):
